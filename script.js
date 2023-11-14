de51b6576f4ddc552d60d22435f4237b92c71655
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2023-11-14 08:45:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const extendHex = (shortHex) => {
   // write your code here
+	shortHex = shortHex.replace("#", "");
+
+  if (shortHex.length !== 3) {
+    throw new Error("Invalid short hex code. It should have 3 characters.");
+  }
+
+  const [r, g, b] = shortHex.split("");
+
+  const fullHex = `#${r.repeat(2)}${g.repeat(2)}${b.repeat(2)}`;
+
+  return fullHex.toUpperCase();
 };
 
 // Do not change the code below.
